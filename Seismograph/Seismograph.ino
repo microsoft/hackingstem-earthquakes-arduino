523a9dbe7c70a277eaa43d30d2281450aabfe1d1
Jeremy Franklin-Ross <v-jerfra@microsoft.com>
2018-10-22 09:12:24 -0700

    reviewd seismo, added license, added readme, moved tmd

--- Seismograph/Seismograph.ino
+++ Seismograph/Seismograph.ino
@@ -1,19 +1,31 @@
-/*
-   Seisomograph code for use with the Tuned Mass Damper lesson plan
-   Available from the Microsoft Education Workshop at http://aka.ms/hackingSTEM
-
-   This projects uses an Arduino UNO microcontroller board. More information can
-   be found by visiting the Arduino website: https://www.arduino.cc/en/main/arduinoBoardUno
-
-   The Seismograph reading is read from analog pin A0 and the results are sent out the serial port.
-   Data can be visualized in Microsoft Excel using the Project Cordoba add-in.
-
-   2017 Microsoft EDU Workshop
-
-*/
+//===----------------__ Hacking STEM Seismograph Arduino __------------===//
+// For use with the Using Computational Thinking to Understand Earthquakes 
+// Seisomograph lesson plan available from Microsoft Education Workshop 
+// at http://aka.ms/hackingSTEM
+//
+// Overview:
+//
+//   This projects uses an Arduino UNO microcontroller board. More 
+//   information can be found by visiting the Arduino website: 
+//   https://www.arduino.cc/en/main/arduinoBoardUno
+//
+//   The Seismograph reading is read from analog pin A0 and the results 
+//   are sent out the serial port. Data can be visualized in Microsoft 
+//   Excel using the Project Cordoba add-in.
+//
+// This project uses an Arduino UNO microcontroller board, information at:
+// https://www.arduino.cc/en/main/arduinoBoardUno
+//
+// Comments, contributions, suggestions, bug reports, and feature requests
+// are welcome! For source code and bug reports see:
+// http://github.com/[TODO github path to Hacking STEM]
+//
+// Copyright Microsoft EDU Workshop - HackingSTEM
+// MIT License terms detailed in LICENSE.txt 
+//===----------------------------------------------------------------------===//
 
 int sensorReading = 0;
-int dataSpeed = 75; //Change this variable to slow down or speed up the rate data is fed into Excel
+int dataSpeed = 75; //Rate in milliseconds data is fed into Excel
 
 // Setup function where we initialize serial communications.
 void setup()
@@ -21,25 +33,24 @@ void setup()
   // Enable serial communications.
   Serial.begin(9600);
   pinMode (A0, INPUT);
-  //analogReference(INTERNAL);
 }
 
 void loop()
 {
-  sensorReading = analogRead(A0) * 5 ;
+  sensorReading = analogRead(A0) * 5 ; // 5 volt is our reference voltage
 
   sensorReading = sensorReading;
   sendSerialData(); // Print out the sensor reading.
-  //delay(75);
+// TODO why don't we pause between sends?
 
-  sensorReading = -sensorReading;
+  sensorReading = -sensorReading;  // TODO why are we sending negative?
   sendSerialData();     // Print out the sensor reading.
   delay(dataSpeed);
 }
 
 void sendSerialData()
 {
-  Serial.print(sensorReading);
+  Serial.print(sensorReading); // TODO why no commas
   Serial.println();
 }
 
