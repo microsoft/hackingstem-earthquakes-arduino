c68ef4bb114af12cf87e311e05e91d23aa4f98fa
David Myka <50000861+damyka@users.noreply.github.com>
2021-06-17 14:32:45 -0700

    added project link

--- Seismograph/Seismograph.ino
+++ Seismograph/Seismograph.ino
@@ -1,15 +1,16 @@
 /*
    Seisomograph code for use with the Tuned Mass Damper lesson plan
-   Available from the Microsoft Education Workshop at http://aka.ms/hackingSTEM
-
+   Available from the Microsoft Education Workshop at 
+   https://www.microsoft.com/en-us/education/education-workshop/seismograph.aspx
+   
    This projects uses an Arduino UNO microcontroller board. More information can
    be found by visiting the Arduino website: https://www.arduino.cc/en/main/arduinoBoardUno
 
    The Seismograph reading is read from analog pin A0 and the results are sent out the serial port.
    Data can be visualized in Microsoft Excel using the Microsoft Data Streamer AddIn for Excel.
 
-   2017 Microsoft EDU Workshop
-
+   David Myka, 2017 Microsoft EDU Workshop
+   http://aka.ms/hackingSTEM
 */
 
 int sensorReading = 0;
